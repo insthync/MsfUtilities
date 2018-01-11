63fd267ab4d7517b765bba8c13ccaf2be1c11bf4
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-11 16:03:33 +0700

    Add generic msf client manager

---
+++ Scripts/CoroutineUtils.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoroutineUtils
+{
+
+    public static IEnumerator StartWaiting(float time,
+        System.Action doneCallback,
+        float increment, System.Action<float> incrementCallback,
+        bool countUp = true)
+    {
+        float timeElapsed = 0f;
+        float timeRemaining = time;
+
+        while (timeRemaining > 0f)
+        {
+            yield return new WaitForSeconds(increment);
+            timeRemaining -= increment;
+            timeElapsed += increment;
+            incrementCallback.Invoke(countUp ? timeElapsed : timeRemaining);
+        }
+        doneCallback.Invoke();
+    }
+
+    public static IEnumerator StartWaiting(float time, System.Action doneCallback)
+    {
+        yield return new WaitForSeconds(time);
+        doneCallback.Invoke();
+    }
+}
---
+++ Scripts/MsfClientManager.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Barebones.Logging;
+using Barebones.MasterServer;
+
+public class MsfClientManager : MonoBehaviour
+{
+    public static MsfClientManager Singleton { get; protected set; }
+    [Header("Connection to Master Server")]
+    [Tooltip("Address to the server")]
+    public string masterIp = "127.0.0.1";
+    [Tooltip("Port of the server")]
+    public int masterPort = 5000;
+    [Tooltip("If true, ip and port will be read from cmd args")]
+    public bool readMasterServerAddressFromCmd = true;
+    [Tooltip("If true, will try to connect on the Start()")]
+    public bool connectToMasterOnStart = true;
+    [Tooltip("If true, will try to connect on failed")]
+    public bool reconnectToMasterOnFailed = true;
+    public float connectToMasterTimeout = 10f;
+
+    public string Status { get; protected set; }
+    protected BmLogger Logger = Msf.Create.Logger(typeof(MsfClientManager).Name);
+    protected IEnumerator backgroundCoroutine;
+
+    private void Awake()
+    {
+        if (Singleton != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Singleton = this;
+
+        // In case this object is not at the root level of hierarchy
+        // move it there, so that it won't be destroyed
+        if (transform.parent != null)
+            transform.SetParent(null, false);
+
+        DontDestroyOnLoad(gameObject);
+
+        if (readMasterServerAddressFromCmd)
+        {
+            // If master IP is provided via cmd arguments
+            if (Msf.Args.IsProvided(Msf.Args.Names.MasterIp))
+                masterIp = Msf.Args.MasterIp;
+
+            // If master port is provided via cmd arguments
+            if (Msf.Args.IsProvided(Msf.Args.Names.MasterPort))
+                masterPort = Msf.Args.MasterPort;
+        }
+    }
+
+    private void Start()
+    {
+        if (connectToMasterOnStart)
+            ConnectToMasterServer();
+    }
+
+    protected void StopBackgroundCoroutine()
+    {
+        if (backgroundCoroutine != null)
+        {
+            StopCoroutine(backgroundCoroutine);
+            backgroundCoroutine = null;
+        }
+    }
+
+    public void ConnectToMasterServer()
+    {
+        Status = string.Empty;
+
+        // connect to the master server
+        Msf.Connection.Connected += OnMasterServerConnected;
+        Msf.Connection.Disconnected += OnMasterServerDisconnected;
+
+        backgroundCoroutine = CoroutineUtils.StartWaiting(connectToMasterTimeout,
+            () => { OnMasterServerConnectionFailed("Timed out"); },
+            1f,
+            (time) => { Status = string.Format("Trying to connect {0}s", time); });
+        StartCoroutine(backgroundCoroutine);
+
+        Logger.Info(string.Format("Connecting to master on {0}:{1}", masterIp, masterPort));
+        Msf.Connection.Connect(masterIp, masterPort);
+    }
+
+    protected virtual void OnMasterServerConnected()
+    {
+        StopBackgroundCoroutine();
+        Status = string.Empty;
+        // TODO: Poll any events
+    }
+
+    protected virtual void OnMasterServerDisconnected()
+    {
+        StopBackgroundCoroutine();
+        Status = string.Empty;
+        // TODO: Poll any events
+    }
+
+    private void OnMasterServerConnectionFailed(string errorMsg)
+    {
+        Status = string.Format("Master server connection failed: {0}", errorMsg);
+
+        Msf.Connection.Connected -= OnMasterServerConnected;
+        Msf.Connection.Disconnected -= OnMasterServerDisconnected;
+
+        if (reconnectToMasterOnFailed)
+            ConnectToMasterServer();
+    }
+
+    public void LogInAsGuest()
+    {
+        if (!Msf.Connection.IsConnected)
+        {
+            Logger.Error("[LogInAsGuest] master server not connected");
+            return;
+        }
+
+        Msf.Client.Auth.LogInAsGuest((accountInfo, errorMsg) =>
+        {
+            if (accountInfo == null)
+                OnLoginFailed(errorMsg);
+            else
+                OnLoggedIn(Msf.Client.Auth.AccountInfo.Username);
+        });
+    }
+
+    public void Login(string username, string password)
+    {
+        if (!Msf.Connection.IsConnected)
+        {
+            Logger.Error("[Login] master server not connected");
+            return;
+        }
+
+        Msf.Client.Auth.LogIn(username, password, (accountInfo, errorMsg) =>
+        {
+            if (accountInfo == null)
+                OnLoginFailed(errorMsg);
+            else
+                OnLoggedIn(Msf.Client.Auth.AccountInfo.Username);
+        });
+    }
+
+    protected virtual void OnLoggedIn(string username)
+    {
+        StopBackgroundCoroutine();
+        Status = string.Format("Logged in: {0}", username);
+    }
+
+    protected virtual void OnLoginFailed(string errorMsg)
+    {
+        Status = string.Format("Login failed: {0}", errorMsg);
+    }
+
+    public void Register(string username, string password, string email)
+    {
+        if (!Msf.Connection.IsConnected)
+        {
+            Logger.Error("[Register] master server not connected");
+            return;
+        }
+
+        var registerData = new Dictionary<string, string>
+        {
+            { "username", username },
+            { "password", password },
+            { "email", email }
+        };
+
+        Msf.Client.Auth.Register(registerData, (success, errorMsg) =>
+        {
+            if (!success)
+                OnRegisterFailed(errorMsg);
+            else
+                OnRegistered();
+        });
+    }
+
+    protected virtual void OnRegistered()
+    {
+        StopBackgroundCoroutine();
+        Status = string.Format("Registered");
+    }
+
+    protected virtual void OnRegisterFailed(string errorMsg)
+    {
+        Status = string.Format("Register failed: {0}", errorMsg);
+    }
+}
